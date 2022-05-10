f6bab868a171ab2bb8e5fb9938a747fbb7189b32
Eric Lantz <77892536+ericmlantz@users.noreply.github.com>
2022-05-10 11:42:40 -0400

    added AuthController and AuthRouter files

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -3,4 +3,8 @@ const UserRouter = require('./UserRouter')
 const InterestRouter = require('./InterestRouter')
 const PageRouter = require('./PageRouter')
 
+Router.use('/users', UserRouter)
+Router.use('/interests', InterestRouter)
+Router.use('/pages', PageRouter)
+
 module.exports = Router
