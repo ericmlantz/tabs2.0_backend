ebde78c2456dbb25de5a1bcd6c65c2a56bff08c8
Eric Lantz <77892536+ericmlantz@users.noreply.github.com>
2022-05-16 08:13:48 -0400

    Fixed delete User

--- migrations/20220510135447-create-user.js
+++ migrations/20220510135447-create-user.js
@@ -10,6 +10,7 @@ module.exports = {
       },
       name: {
         type: Sequelize.STRING,
+        allowNull: false
       },
       email: {
         type: Sequelize.STRING,
--- models/interest.js
+++ models/interest.js
@@ -24,7 +24,6 @@ module.exports = (sequelize, DataTypes) => {
         model: 'users',
         key: 'id'
       }
-
     }
   }, {
     sequelize,
